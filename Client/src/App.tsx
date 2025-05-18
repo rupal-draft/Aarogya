d95578ce88c882c61003a0efe5fbcd2e8906aa66
Rupal Paul <rupalpaulgspr@gmail.com>
2025-05-18 13:41:39 +0530

    Settig up react for client

---
+++ Client/src/App.tsx
@@ -0,0 +1,11 @@
+import './assets/App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-pink-500">
+      <h1 className="text-red text-4xl font-bold">Tailwind v4 is working! 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
