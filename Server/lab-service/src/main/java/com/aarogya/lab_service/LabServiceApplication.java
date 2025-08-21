a0cb45d90f9a9778306dddf5d1284a1eb7f80648
Rupal Paul <rupalpaulgspr@gmail.com>
2025-08-21 13:42:16 +0530

    Added config,advices,exceptions and auth context to lab service

---
+++ Server/lab-service/src/main/java/com/aarogya/lab_service/LabServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aarogya.lab_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabServiceApplication.class, args);
+	}
+
+}
---
+++ Server/lab-service/src/test/java/com/aarogya/lab_service/LabServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarogya.lab_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
