bac45a3bd99d19ff401f37d67886b99f2fbe03bf
Rupal Paul <rupalpaulgspr@gmail.com>
2025-08-23 11:56:31 +0530

    Setting up patient management service

---
+++ Server/patient-management-service/src/main/java/com/aarogya/patient_management_service/PatientManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aarogya.patient_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementServiceApplication.class, args);
+	}
+
+}
---
+++ Server/patient-management-service/src/test/java/com/aarogya/patient_management_service/PatientManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarogya.patient_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
